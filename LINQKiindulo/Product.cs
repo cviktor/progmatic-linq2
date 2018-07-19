febaed3fd22cf75fffec098990e38fae1d58976b
Czeglédi Viktor <czegledi.viktor@gmail.com>
2018-07-19 14:31:57 +0200

    Initial commit

---
+++ LINQKiindulo/Product.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQKiindulo
+{
+    [DataContract(Namespace = "CSharpHalado")]
+    public class Product
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int CategoryID { get; set; }
+
+        [DataMember]
+        public Category Category { get; set; }
+
+        [DataMember]
+        public string QuantityPerUnit { get; set; }
+
+        [DataMember]
+        public decimal UnitPrice { get; set; }
+
+        [DataMember]
+        public short UnitsInStock { get; set; }
+
+        [DataMember]
+        public short UnitsOnOrder { get; set; }
+
+        [DataMember]
+        public bool Discontinued { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name}: {UnitPrice}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            /*
+            Amit eredetileg írtunk hibás mert ha az obj nem Product 
+            vagy abból származó osztály példánya
+            akkor a castolás kivételt dob
+            helyes megoldáshoz még ez az if is kell, 
+            mert ez ellenőrzi hogy lehet-e Productra castolni
+            ha nem lehet akkor biztos nem egyenlőek
+            */
+            if (!(obj is Product)) return false;
+
+            Product p = (Product)obj;
+            return p.Name == this.Name;
+        }
+    }
+}
--- LINQKiindulo/Program.cs
+++ LINQKiindulo/Program.cs
@@ -1,17 +1,48 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace LINQKiindulo
 {
     class Program
     {
+        static List<Category> categories;
+
+        static List<Product> products;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Init();
+
             Console.ReadKey();
+        }
+
+        static void Init()
+        {
+            using (var catFile = XmlReader.Create(@"..\..\categories.xml"))
+            {
+                var dcs = new DataContractSerializer(typeof(List<Category>));
+                categories = (List<Category>)dcs.ReadObject(catFile);
+                foreach (var c in categories)
+                {
+                    c.Products = new List<Product>();
+                }
+            }
+
+            using (var prodFile = XmlReader.Create(@"..\..\products.xml"))
+            {
+                var dcs = new DataContractSerializer(typeof(List<Product>));
+                products = (List<Product>)dcs.ReadObject(prodFile);
+                foreach (var prod in products)
+                {
+                    prod.Category = categories.Single(c => c.CategoryID == prod.CategoryID);
+                    prod.Category.Products.Add(prod);
+                }
+            }
         }
     }
 }
