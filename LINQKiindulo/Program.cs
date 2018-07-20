5d7eeac5eeed0045b4e1f485b8af503db2ce21a9
Czeglédi Viktor <czegledi.viktor@gmail.com>
2018-07-20 10:19:52 +0200

    Késői kiértékelés példák

--- LINQKiindulo/Program.cs
+++ LINQKiindulo/Program.cs
@@ -31,10 +31,13 @@ namespace LINQKiindulo
             var numbers = new List<int> { 2, 3, 4, 5, 6 };
             var evenNumbers = numbers.Where(n => n % 2 == 0).Where(n => n > 3);
             var evenList = evenNumbers.ToList();
-
+            evenList.Add(12);
+            numbers.Add(8);
             evenList.WriteAll("Páros lista");
             evenNumbers.WriteAll("Páros számok");
+
             numbers.Add(10);
+
             evenList.WriteAll("Páros lista 2");
             evenNumbers.WriteAll("Páros számok 2");
 
